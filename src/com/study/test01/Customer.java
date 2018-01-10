5c5205c467d778a34a35dfa72eeebede21880023
ZonJanLee <ZonJanLee>
2018-01-10 15:00:54 +0800

    java 基础笔记

---
+++ src/com/study/test01/Customer.java
@@ -0,0 +1,23 @@
+package com.wuzhou.Test01;
+
+public class Customer {
+	private String name;
+	private Account account;
+	
+	public Customer(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
+}
